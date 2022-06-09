d6c96a399b391494ca611ccf51d9156fc75b49a9
giwoong kim <pooh_hoop@naver.com>
2022-06-09 17:49:48 +0900

    '0609'

---
+++ Rpa_macro/src/Imsi.java
@@ -0,0 +1,91 @@
+import java.awt.Robot;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.ss.formula.functions.Value;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.awt.*; 
+
+public class Imsi {
+
+	
+	public static void test() {
+		try {
+			
+			Robot bot =  new Robot();	
+			  bot.mouseMove(1466,222); bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);//
+			  //왼쪽누르기 
+			  bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);// 떄기
+			  
+			  bot.keyPress( KeyEvent.VK_CONTROL ); bot.keyPress( KeyEvent.VK_A );
+			  bot.keyRelease(KeyEvent.VK_CONTROL); bot.keyRelease( KeyEvent.VK_A );
+			  
+			  
+			  bot.keyPress( KeyEvent.VK_CONTROL ); bot.keyPress( KeyEvent.VK_C );
+			  bot.keyRelease( KeyEvent.VK_CONTROL ); bot.keyRelease( KeyEvent.VK_C );
+			  
+			  
+			  //현재 갯수값 가져오기 
+			  Clipboard clipboard =Toolkit.getDefaultToolkit().getSystemClipboard(); 
+			  Transferable contents = clipboard.getContents(clipboard);   
+			  String current = (String)contents.getTransferData(DataFlavor.stringFlavor);
+			  int aa = Integer.parseInt(current);
+			 			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	public static int exceldata(int aa,int params) throws IOException {
+		int num = 	params;
+		//엑셀 데이터 값 가져오기
+		String path = Imsi.class.getResource("").getPath();
+		FileInputStream file = new FileInputStream(path+"rotto.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		XSSFRow row = null;
+		XSSFCell cell = null;
+		//총 row 수
+		int row_num =  sheet.getPhysicalNumberOfRows();
+		// 시트 수량 	
+		row = sheet.getRow(num);
+		cell = row.getCell(4);
+		int bb = (int) cell.getNumericCellValue();
+					
+		return bb;
+			
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		while (true) {
+			Thread.sleep(5000);
+			test();
+		}
+		
+	
+		
+	}
+	
+	
+	
+
+}
